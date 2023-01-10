9adb7c481999611d155edb06fbddd2541cc584e9
KadeCS <KadePcGames@gmail.com>
2023-01-09 19:59:50 -0800

    Mod Frame Initial Commit

---
+++ README.md
@@ -0,0 +1,132 @@
+# ModFrame
+> A very easy way to create modfiles in Average4K (heavily inspired by NotITG's [Mirin Template](https://xerool.github.io/notitg-mirin/))
+
+Mod frame is a seperate *.lua* file that is put in the same folder as your *mod.lua* file. 
+This file has lots of ways to create visually stunning mods, that sync well.
+
+## How to use
+
+Copy and paste [modFrame.lua](modFrame.lua) into your folder, and make sure your main starting code looks like this:
+
+```lua
+function create()
+    -- This dofile runs modFrame
+    dofile(formCompletePath("modFrame.lua"))
+
+    ModFrame.create() -- this creates all the mods we can use
+end
+
+function editor_scroll() -- for previewing in the mod editor
+    ModFrame.editor_scroll()
+end
+
+function update(beat) -- actually run our mods
+    ModFrame.update(beat)
+end
+```
+
+Now that you have the main functions set up, you can now create some cool mods!
+
+## ModEase
+
+**me**, short for modease;
+
+Is used for the easing of mods. Think of it like *"activateMod"* and *"activateModMap"* all in one function.
+
+After the *ModFrame.create()* function, you can use *me* to start easing mods.
+
+Example:
+
+```lua
+--[[
+    it might be weird seeing {}'s instead of ()'s but all that its really doing is letting us create
+    a table, so we can pass it into the function as the argument. It's basically equivalent to, func({tableValue, tableValue, etc})
+
+    The properties for this table are as follows:
+    Mod Name,
+    Start Value (can be nil. If so, it will take the current value of the mod at runtime),
+    End Value,
+    Tween Start,
+    Tween End,
+    Easing,
+    Column (can be nil/-1, if so. It will not use the column)
+]]--
+
+-- To move the entire playfield to the right, it'd follow like this:
+
+me{'amovex', nil, 250, 1, 4, 'outcubic'}
+```
+
+## Set
+
+The instant version of modease.
+
+It's extremely simple, and I dont really have to explain much.
+
+Example:
+
+```lua
+--[[
+    Properties are:
+    Mod Name,
+    End Value,
+    Beat,
+    Column (can be nil/-1, if so. It will not use the column)
+]]--
+
+-- Instantly set the playfield 250 pixels to the right
+
+set{'amovex',250,2.9}
+```
+
+## DefineMod
+
+defineMod is a very special function that lets you define your own mods!
+
+In which you can then use in *me*/*set*
+
+Here is a semi-tutorial from the code because I am lazy.
+
+```lua
+--[[
+    defineMod defines a mod that you can then set the value to using setModValue/setModValueColumn
+
+    defineMod{'modName', 0, function(beat, perc, me, plr)
+            -- Your code
+            -- "me" is simply a reference to your custom mod, which contains a .value property you can set
+        end, false, false}
+
+    The 0 is the default value.
+
+    Setting the function to "nil" will make it automaticly set the "value" property
+
+    The 2nd to last argument is for Average4K and if the mod you defined is a Built-In mod, as setting that argument to true
+    will try to set its mod property in game. It will do nothing if it was not found though.
+
+    You also have a columns array, which is what the last arugmnet is specifying. Its if this mod affects only one column.
+    If you set this to true, the ModFramework will try to read the columns array and set each columns property.
+
+    This is probably only good for Built-In mods so you should probably ignore it.
+
+    By the way, the defaults for the last 2 arguments will default to false if set to nil. 
+    So you can completely ignore them if you want. Just wanted to explain it :)
+]]--
+
+-- Example:
+
+defineMod{'myMod',0, function(beat, perc, me, plr)
+    -- Set column one's confusion to 180
+    setModValueColumn('confusion', 1, plr, 180)
+end, false, false}
+```
+
+## Other functions
+
+There are some other functions that I will now list here:
+
+| Function | Description |
+| --- |----------- |
+| getModValue | Get a mods value |
+| setModValue | Set a mods value |
+| setModValueColumn | Set a mods value on a column |
+| addPlayer | Add a player in Average4K, and also append it's id to the **playfields** global |
---
+++ modFrame.lua
@@ -0,0 +1,346 @@
+--[[
+    This is obviously inspired by mirin template from NotITG, so go check them out :) 
+    https://xerool.github.io/notitg-mirin/
+
+    oh and side note, probably don't touch this file unless you know what you are doing.
+
+    consider this the "don't touch kiddo" warning lmao
+]]--
+
+
+-- Helper
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+ end
+
+ModFrame = {}
+
+plr = 0
+
+playfields = {
+    0
+}
+
+-- setup arrays
+
+--[[
+    Structure
+
+    name
+    value
+    playfieldValues
+    func
+    setBack
+    useColumns
+    columns
+    default
+    
+]]--
+
+
+ModFrame.mods = {
+
+}
+
+--[[
+    Structure
+
+    modName
+    startValue - if nil, assign the current value as that start value
+    endValue
+    tweenStart
+    tweenLen
+    easing
+    complete
+    column
+    playfield
+
+]]
+
+ModFrame.activeMods = {
+
+}
+
+--[[
+    defineMod defines a mod that you can then set the value to using setModValue/setModValueColumn
+
+    defineMod{'modName', 0, function(beat, perc, me, plr)
+            -- Your code
+            -- "me" is simply a reference to your custom mod, which contains a .value property you can set
+        end, false, false}
+
+    The 0 is the default value.
+
+    Setting the function to "nil" will make it automaticly set the "value" property
+
+    The 2nd to last argument is for Average4K and if the mod you defined is a Built-In mod, as setting that argument to true
+    will try to set its mod property in game. It will do nothing if it was not found though.
+
+    You also have a columns array, which is what the last arugmnet is specifying. Its if this mod affects only one column.
+    If you set this to true, the ModFramework will try to read the columns array and set each columns property.
+
+    This is probably only good for Built-In mods so you should probably ignore it.
+
+    By the way, the defaults for the last 2 arguments will default to false if set to nil. 
+    So you can completely ignore them if you want. Just wanted to explain it :)
+]]--
+function defineMod(n)
+    local m = {}
+    m.name = n[1]
+    m.func = n[2]
+    m.setBack = n[3]
+    m.useColumns = n[4]
+    m.value = n[5]
+    m.playfieldValues = {}
+    m.columns = {}
+    m.default = m.value
+    table.insert(ModFrame.mods,m)
+end
+
+function addPlayer()
+    table.insert(playfields, createPlayfield())
+end
+
+--[[
+    Smootly interperlates to an end value from a start value.
+
+    me{
+        modName = 'amovex',
+        startValue = nil, -- Get current value
+        endValue = 250,
+        tweenStart = 1,
+        tweenLen = 4,
+        easing = 'outcubic',
+        column = -1 -- no column
+    }
+
+    Note: Do not add the "modName =" etc.
+
+    A real example looks smth like this:
+    me{'amovex',0,250,3,1,'outcubic',-1}
+]]--
+function me(n)
+    local m = {}
+    m.modName = n[1]
+    m.startValue = n[2]
+    m.endValue = n[3]
+    m.tweenStart = n[4]
+    m.tweenLen = n[5]
+    m.easing = n[6]
+    m.column = n[7]
+    if m.column == nil then
+        m.column = -1
+    end
+    m.playfield = plr
+
+    table.insert(ModFrame.activeMods,m)
+end
+
+--[[
+    Immediately sets a mods value
+
+    ModFrame.set{
+        modName = 'amovex',
+        endValue = 250,
+        tweenStart = 1,
+        column = -1 -- no column
+    }
+
+    Note: Do not add the "modName =" etc.
+
+    A real example looks smth like this:
+    set{'aconfusion',0,1,-1}
+]]--
+
+function set(n)
+    local m = {}
+    m.modName = n[1]
+    m.endValue = n[2]
+    m.tweenStart = n[3]
+    m.column = n[4]
+    if m.column == nil then
+        m.column = -1
+    end
+    m.tweenLen = 0
+    m.startValue = nil
+    m.playfield = plr
+    m.easing = 'linear'
+    table.insert(ModFrame.activeMods,m)
+end
+
+function ModFrame.create()
+    defineMod{'drunk', nil, true, false, 0}
+    defineMod{'drunkCol', nil, true, true, 0}
+    defineMod{'tipsy', nil, true, false, 0}
+    defineMod{'tipsyCol', nil, true, true, 0}
+    defineMod{'wave', nil, true, false, 0}
+    defineMod{'waveCol', nil, true, true, 0}
+    defineMod{'aconfusion', nil, true, false, 0}
+    defineMod{'confusion', nil, true, true, 0}
+    defineMod{'amovex', nil, true, false, 0}
+    defineMod{'movex', nil, true, true, 0}
+    defineMod{'amovey', nil, true, false, 0}
+    defineMod{'movey', nil, true, true, 0}
+    defineMod{'reverse', nil, true, true, 0}
+    defineMod{'dizzy', nil, true, false, 0}
+    defineMod{'dizzyCol', nil, true, true, 0}
+    defineMod{'mini', nil, true, false, 0.5}
+    defineMod{'miniCol', nil, true, true, 0.5}
+    defineMod{'stealthWhite', nil, true, true, 0}
+    defineMod{'stealthOpacity', nil, true, true, 1}
+    defineMod{'stealthReceptorOpacity', nil, true, true, 1}
+    defineMod{'pathAlpha', nil, true, false, 0}
+    defineMod{'pathDensity', nil, true, false, 1}
+
+    consolePrint("ModFrame Loaded! Created by Kade :)")
+end
+
+function getModValue(name)
+    for index, value in ipairs(ModFrame.mods) do
+        if value.name == name then
+            return value.value
+        end
+    end
+    return 0
+end
+
+
+function setModValue(name, v)
+    for index, value in ipairs(ModFrame.mods) do
+        if value.name == name then
+            value.value = v
+        end
+    end
+end
+
+function setModValueColumn(name, column, pid, v)
+    for index, value in ipairs(ModFrame.mods) do
+        if value.name == name then
+            if value.columns[pid] == nil then
+                value.columns[pid] = {}
+            end
+            value.columns[pid][column] = {value = v}
+        end
+    end
+end
+
+function getMod(name)
+    for index, value in ipairs(ModFrame.mods) do
+        if value.name == name then
+            return value
+        end
+    end
+    return nil
+end
+
+function getModIndex(name)
+    for index, value in ipairs(ModFrame.mods) do
+        if value.name == name then
+            return index
+        end
+    end
+    return 0
+end
+
+function ModFrame.update(beat)
+    for index, m in ipairs(ModFrame.activeMods) do
+        if beat > m.tweenStart and not m.complete then
+            
+            local md = getMod(m.modName)
+
+            if md == nil then
+                consolePrint('[ModFrame] The mod ' .. m.modName .. ' could not be found!')
+            else
+
+                local dur = (beat - m.tweenStart)
+
+                local t = 0
+
+                if m.tweenLen == 0 then
+                    t = 1
+                else
+                    t = (dur / m.tweenLen)
+                end
+
+                if beat > m.tweenStart + m.tweenLen then
+                    ModFrame.activeMods[index].complete = true
+                    t = 1
+                end
+
+                if m.startValue == nil then
+                    m.startValue = md.value
+                end
+
+                local value = tween(m.startValue, m.endValue, t, m.easing)
+
+                if md.useColumns then
+                    if md.columns[m.playfield] == nil then
+                        md.columns[m.playfield] = {}
+                    end
+                    if m.column ~= -1 and m.column ~= nil then
+                        md.columns[m.playfield][m.column] = {value = value}
+                    end
+                else
+                    md.value = value
+                    md.playfieldValues[m.playfield] = value
+                end
+
+                if md.func ~= nil then
+                    md.func(beat, value, md, m.playfield)
+                end
+            end
+        end
+    end
+
+    for index1, mod in ipairs(ModFrame.mods) do
+        if mod.setBack then
+            for pn = 0,#playfields - 1 do
+                local value = mod.value
+
+                if mod.playfieldValues[pn] ~= nil then
+                    value = mod.playfieldValues[pn]
+                end
+                
+                if mod.useColumns then
+                    local c = mod.columns[pn]
+                    if c ~= nil then
+                        for i = 1, 4 do
+                            local col = c[i]
+                            if col ~= nil then
+                                setModProperty(mod.name, pn, i - 1, col.value)
+                            else
+                                setModProperty(mod.name, pn, i - 1, value)
+                            end
+                        end
+                    else
+                        setModProperty(mod.name, pn, -1, value)
+                    end
+                else
+                    setModProperty(mod.name, pn, -1, value)
+                end
+            end
+        end
+    end
+end
+
+function ModFrame.editor_scroll()
+    for index, m in ipairs(ModFrame.mods) do
+        m.value = m.default
+        m.columns = {}
+        m.playfieldValues = {}
+    end
+    for index, m in ipairs(ModFrame.activeMods) do
+        m.complete = false
+    end
+
+    ModFrame.update(getBeat())
+end
